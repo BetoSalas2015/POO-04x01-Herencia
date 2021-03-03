2fd4898ff5e4ea8c06c2d16aae30a799ef54dca3
Roberto Salazar <roberto.salazar@upaep.mx>
2021-03-02 20:22:00 -0600

    Sesión 19 - 02/03/2021

--- Circulo.java
+++ Circulo.java
@@ -8,16 +8,39 @@
 public class Circulo extends Punto
 {
     // instance variables - replace the example below with your own
-
+    private int radio;
 
     /**
      * Constructor for objects of class Circulo
      */
-    public Circulo(int x, int y)
+    public Circulo(int x, int y, int radio)
     {
          //initialise instance variables
-        super(x,y);
+         super(x, y);
+         setRadio(radio);
+    }
+    
+    public void setRadio(int r)
+    {
+        radio = (r > 0) ? r : 1;
+    }
+    
+    public int getRadio()
+    {
+        return radio;
+    }
+    
+    public String toString()
+    {
+        return "Centro " + super.toString() + ", radio: " + radio;
     }
 
 
 }
+
+
+
+
+
+
+
